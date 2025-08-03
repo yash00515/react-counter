ea4d0a1a5f884ef9394d89bf4111c26e8fb08a94
yash00515 <yashgajipara16@gmail.com>
2025-08-03 12:35:29 +0530

    Initial React Counter project upload

---
+++ gcg.jsx
@@ -0,0 +1,50 @@
+import React, { useRef, useState } from 'react';
+import "./Counter.css";
+
+const Counter = () => {
+  const [num, setNum] = useState(0);
+  const reference = useRef();
+
+  const handle = () => {
+    const value = parseInt(reference.current.value);
+    if (!isNaN(value)) {
+      setNum(value);
+    }
+  };
+
+  const increase = () => {
+    setNum(num + 1);
+  };
+
+  const decrease = () => {
+    setNum(num - 1);
+  };
+
+  const reset = () => {
+    setNum(0);
+  };
+
+  return (
+    <>
+      <button className='btn'>COUNTER APP</button>
+      <br />
+
+      <h3>Enter the Value </h3>
+      <input type="number" className='valuebox' ref={reference} placeholder='0' />
+      <br /><br />
+
+      <button className='formbtn' onClick={handle}>Submit</button>
+      <br /><br /><br />
+
+      <h2 className='op'>Output</h2>
+      <h2>Value: {num}</h2>
+      <br /><br />
+
+      <button onClick={increase} className="final">Increment +</button>
+      <button onClick={decrease} className="final">Decrement -</button>
+      <button onClick={reset} className='final'>Reset</button>
+    </>
+  );
+};
+
+export default Counter;
---
+++ main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+
+import Counter from './Counter.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <Counter />
+)
