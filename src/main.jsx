b8a3c5b67a517203f944cca06f3b0ff827011929
yash00515 <yashgajipara16@gmail.com>
2025-08-27 18:28:07 +0530

    First Commit - Bank Form Project

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,11 @@
- import { createRoot } from 'react-dom/client'
+  
+import { createRoot } from 'react-dom/client'
 import './index.css'
-
-// Import components
-import Navbar from './Navbar'
-import Hero from './Hero'
-import Hero3 from './Hero3'
-import Cards from './Cards'
-import Scard from './Scard'
-import Cards2 from './Cards2'
-import Footer from './Footer'
-import Last from './Last'
-// import Bankform from './Bankform.jsx' // uncomment if you want to use Bankform instead
-
-// Font Awesome (better include in index.html, not here)
-// <link ... /> should be placed inside public/index.html head tag
+ 
+import Bankform from './Bankform.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <Navbar />
-    <Hero />
-    <Hero3 />
-    <Cards />
-    <Scard />
-    <Cards2 />
-    <Last />
-    <Footer />
  
-  </>
+    <Bankform />
+ 
 )
